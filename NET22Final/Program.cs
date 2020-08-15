316ffb8f1a92bc2fdca82640482662f182e230c4
deeptinirwal <66492566+deeptinirwal@users.noreply.github.com>
2020-08-15 11:16:42 -0400

    Update Program.cs

--- NET22Final/Program.cs
+++ NET22Final/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace NET22Final
+namespace NET22FinalGeneric
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is Generics");
+            var numbers = new Generics<int>(3);
+            Console.WriteLine("Value is : " + numbers.AddItems());
         }
+    }
+
+    public class Generics<T> where T : struct
+    {
+        private object _value;
+
+        public Generics()
+        {
+
+        }
+        public Generics(T value)
+        {
+            _value = value;
+        }
+
+        public bool HasValue
+        {
+            get { return _value != null; }
+        }
+
+        public T AddItems()
+        {
+            if (HasValue)
+                return (T)_value;
+
+            return default(T);
+        }
+
+
     }
 }
