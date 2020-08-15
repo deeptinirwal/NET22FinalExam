33d3698e5ee33191ddea7f7ccfe4a2cffe062cba
deeptinirwal <66492566+deeptinirwal@users.noreply.github.com>
2020-08-15 10:59:49 -0400

    first commit

---
+++ NET22Final/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NET22Final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
